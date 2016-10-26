e52b108358b7d060b2be1a60fdce496d8fddf32d
Ryan T <ryant1234@gmail.com>
2016-10-27 09:45:03 +1300

    Initial Commit

---
+++ GigHub/Controllers/FollowingController.cs
@@ -0,0 +1,46 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class FollowingControllers : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingControllers()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == dto.FolloweeId))
+            {
+                return BadRequest("The following already exists.");
+            }
+            var following = new Following()
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+
+
+        }
+        
+        
+        
+        // GET: Following
+       
+    }
+}
